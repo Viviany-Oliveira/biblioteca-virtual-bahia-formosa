eef81fa5de391042f93facec5d5e543cb00fce1a
marcusviniciusfelipea <marcusviniciusfelipeavelino@gmail.com>
2026-09-10 19:38:26 +0000

    ambiente virtual

--- penalidades/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PenalidadesConfig(AppConfig):
-    name = "penalidades"
---
+++ prazo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrazoConfig(AppConfig):
+    name = "prazo"
